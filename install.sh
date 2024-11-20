ccfcd6c983dd20689f1f9b73a197a57d6ad7af3a
Kristinn J <6684975+kristinn93@users.noreply.github.com>
2024-11-20 10:14:59 +0000

    test applying zsh config

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@
 
 
 # Install oh-my-zsh and plugins
+echo "Installing oh-my-zsh and plugins"
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
 zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
 zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
+cp ./.zshrc ~
